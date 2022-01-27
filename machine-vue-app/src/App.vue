fce9bc52ac7d68f8f94ce2ee27399e289f0e357f
Sadie Louise Bartholomew <sadie.bartholomew@ncas.ac.uk>
2022-01-27 23:25:13 +0000

    Add Vue.JS components: root and Machine

---
+++ machine-vue-app/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Machine/>
+</template>
+
+<script>
+import Machine from './components/Machine.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Machine
+  }
+}
+</script>
